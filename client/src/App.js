d3dcd2b48bb7a71a955dcc04153f5bf38bb52a3d
Abdul-Rahime Kadri <abdul.r.kadri@gmail.com>
2023-05-06 15:36:02 -0600

    Add theme provider and css baseline

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,30 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import HomePage from 'scenes/homePage';
 import LoginPage from 'scenes/loginPage';
 import ProfilePage from 'scenes/profilePage';
+import { useMemo } from 'react'; // useMemo is used to prevent unnecessary re-renders
+import { useSelector } from 'react-redux'; // useSelector is used to get data from redux store
+import { CssBaseline, ThemeProvider } from '@mui/material'; // CssBaseline is used to set default CSS
+import { createTheme } from '@mui/material/styles'; // createTheme is used to create a theme
+import { themeSettings } from 'theme'; // themeSettings is used to set theme settings
 
 const App = () => {
+  const mode = useSelector((state) => state.mode); // get mode from redux store
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]); // create theme based on mode
 
   return (
     <div className="app">
       <Router>
-        <Routes>
-          <Route path="/" element={<LoginPage />} /> {/* This is the default route */}
-          <Route path="/home" element={<HomePage />} /> {/* Set homepage route*/}
-          <Route path="/profile/:userId" element={<ProfilePage />} /> {/* Set profile page route */}
-        </Routes>
+        <ThemeProvider theme={theme}> {/* Set theme */}
+          <CssBaseline /> {/* Set default CSS */}
+          <Routes>
+            <Route path="/" element={<LoginPage />} /> {/* This is the default route */}
+            <Route path="/home" element={<HomePage />} /> {/* Set homepage route*/}
+            <Route path="/profile/:userId" element={<ProfilePage />} /> {/* Set profile page route */}
+          </Routes>
+        </ThemeProvider>
       </Router>
     </div>
   );
-}
+};
 
 export default App;
